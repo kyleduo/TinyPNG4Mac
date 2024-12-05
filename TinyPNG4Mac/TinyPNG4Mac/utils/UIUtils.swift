94014c874c35f77725c77d32c086ee04af467d90
kyleduo <kyleduo@gmail.com>
2024-12-06 00:01:01 +0800

    remove unused code

--- TinyPNG4Mac/TinyPNG4Mac/utils/UIUtils.swift
+++ TinyPNG4Mac/TinyPNG4Mac/utils/UIUtils.swift
@@ -8,26 +8,24 @@
 import SwiftUI
 
 struct UIUtils {
-    
-
     static func colorFromHex(_ hex: String) -> Color {
         var cleanedHex = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
-        
+
         // Remove '#' if it's present
         if cleanedHex.hasPrefix("#") {
             cleanedHex.removeFirst()
         }
-        
+
         // Ensure the hex string is valid
         guard cleanedHex.count == 6 || cleanedHex.count == 8 else {
             return Color.gray // Return gray color for invalid hex
         }
-        
+
         // Add alpha value if not present (defaults to 1.0)
         if cleanedHex.count == 6 {
             cleanedHex += "FF" // Default alpha to full opacity
         }
-        
+
         // Extract RGB and alpha components from hex string
         let scanner = Scanner(string: cleanedHex)
         var hexInt: UInt64 = 0
@@ -38,14 +36,19 @@ struct UIUtils {
             let alpha = Double(hexInt & 0xFF) / 255.0
             return Color(red: red, green: green, blue: blue, opacity: alpha)
         }
-        
+
         return Color.gray // Return gray color for invalid hex
     }
 }
 
-
 extension Color {
     init(hex: String) {
         self = UIUtils.colorFromHex(hex)
+    }
+}
+
+extension View {
+    func padding(vertical: CGFloat, horizontal: CGFloat) -> some View {
+        return padding(EdgeInsets(top: vertical, leading: horizontal, bottom: vertical, trailing: horizontal))
     }
 }
